2d841547dfa5bfa556f32e911ea88137bee27145
Roman Kozhin <kozhin.rm@gmail.com>
2023-07-11 12:13:32 +0300

    Вывод массива

---
+++ hw_4/Ex_29/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19, 2, 4, 9 -> [1, 2, 5, 7, 19, 2, 4, 9 ]
+6, 1, 33, 2, 93, 15, 424, 0-> [6, 1, 33, 2, 93, 15, 424, 0]
+*/
+Console.Clear();
+
+int[] array = new int[8];
+FillArray(array);
+var str = string.Join(" ", array);
+Console.WriteLine(str);
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
