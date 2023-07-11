c8751e9c50a1aef9e860514ec7cbd4b9788475a2
Roman Kozhin <kozhin.rm@gmail.com>
2023-07-11 11:43:30 +0300

    Cумма цифр в числе

---
+++ hw_4/Ex_27/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int DigitSum(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
+Console.WriteLine(DigitSum(number));
