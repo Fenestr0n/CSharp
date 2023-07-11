8e9a972be67db2fd5fe4e8d086ce7ecb3411312e
Roman Kozhin <kozhin.rm@gmail.com>
2023-07-11 11:32:08 +0300

    Возвести число A в натуральную степень B

---
+++ hw_4/Ex_25/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+Console.Write("Введите основание степени: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите показатель степени: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int numPow(int a, int b)
+{
+    int res = 1;
+    for (int i = 0; i < b; i++) res = res * a;
+    return res;
+}
+Console.WriteLine($"Число {a} в степени {b} = {numPow(a, b)}");
