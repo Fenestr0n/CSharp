eb1d380aad1295f7d70a627df77f217e26f8cbc0
Roman Kozhin <kozhin.rm@gmail.com>
2023-07-11 12:36:46 +0300

    Количество чётных чисел в массиве

---
+++ hw_5/Ex_34/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+Console.Clear();
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(size);
+PrintArray(arr);
+Console.WriteLine(CountEven(arr));
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    var str = string.Join(" ", array);
+    Console.WriteLine(str);
+}
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
