49d7f671f6259d87a0bacb3b48c24172db9bd91a
Roman Kozhin <kozhin.rm@gmail.com>
2023-07-11 12:48:22 +0300

    Cумма элементов, стоящих на нечётных позициях

---
+++ hw_5/Ex_36/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(size);
+PrintArray(arr);
+Console.WriteLine(SumOdd(arr));
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    var str = string.Join(" ", array);
+    Console.WriteLine(str);
+}
+
+int SumOdd(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
